b9d880fb5602c7d7464ef9939440c1819a8213cc
Boluwatito Samuel <samuelgabby04@gmail.com>
2025-10-08 21:51:39 -0300

    Third commit

--- Project_scripts/03_date_range_exploration.sql
+++ Project_scripts/03_date_range_exploration.sql
@@ -7,7 +7,7 @@ Purpose:
     - To understand the range and time span of the data.
 
 SQL Functions Used:
-    - MIN(), MAX(), DATEDIFF()
+    - MIN(), MAX(), EXTRACT(), AGE()
 ===============================================================================
 */
 
@@ -15,13 +15,20 @@ SQL Functions Used:
 SELECT 
     MIN(order_date) AS first_order_date,
     MAX(order_date) AS last_order_date,
-    DATEDIFF(yy, '2013-03-16', '2019-09-23')
+    Extract(YEAR FROM MAX(order_date)) - Extract(YEAR FROM MIN(order_date)) AS order_range_year
 FROM gold_fact_sales;
 
 -- Find the youngest and oldest customer based on birthdate
 SELECT
     MIN(birthdate) AS oldest_birthdate,
-    DATEDIFF(YEAR, MIN(birthdate), GETDATE()) AS oldest_age,
+    Extract(YEAR FROM MIN(birthdate)) AS oldest_age,
     MAX(birthdate) AS youngest_birthdate,
-    DATEDIFF(YEAR, MAX(birthdate), GETDATE()) AS youngest_age
+    Extract(YEAR FROM MAX(birthdate) ) AS youngest_age
 FROM gold_dim_customers;
+
+
+SELECT
+    Extract(YEAR FROM AGE( MAX(birthdate)))
+FROM 
+    gold_dim_customers;
+
